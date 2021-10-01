5bb264c3cdb7f54f53952cd6a35739004ccc8166
Osee-Ralantoarison <o.ralanto@gmail.com>
2021-10-01 13:36:32 +0200

    HomePage element

---
+++ screens/HomePage.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import { Card, Button, Icon } from "react-native-elements";
+
+const HomePage = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text h1 style={styles.title}>
+            La danse Hip-Hop
+          </Text>
+          <Text style={styles.description}>
+            La danse hip-hop désigne plusieurs formes de street dance
+            principalement effectuées dans la musique hip-hop et catégorisées
+            dans sa culture. Elle dénombre plusieurs styles de danses tels que
+            le breaking, le locking, la house et le popping créés dans les
+            années 1970 et popularisés par les dance crews aux États-Unis.
+          </Text>
+          <Card>
+            <Card.Title>Débutant</Card.Title>
+            <Card.Divider />
+            <Text style={{ marginBottom: 10 }}>
+              The idea with React Native Elements is more about component
+              structure than actual design.
+            </Text>
+            <Card.Image source={require("../images/Beginner.jpeg")} />
+            <Button
+              icon={<Icon name="code" color="#ffffff" />}
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0,
+              }}
+              title="Commencer le cours débutant"
+            />
+          </Card>
+          <Card>
+            <Card.Title>Intermediaire</Card.Title>
+            <Card.Divider />
+            <Text style={{ marginBottom: 10 }}>
+              The idea with React Native Elements is more about component
+              structure than actual design.
+            </Text>
+            <Card.Image source={require("../images/advanced.jpeg")} />
+            <Button
+              icon={<Icon name="code" color="#ffffff" />}
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0,
+              }}
+              title="Commencer le cours intermediaire"
+            />
+          </Card>
+          <Card>
+            <Card.Title>Avancé</Card.Title>
+            <Card.Divider />
+            <Text style={{ marginBottom: 10 }}>
+              The idea with React Native Elements is more about component
+              structure than actual design.
+            </Text>
+            <Card.Image source={require("../images/Intermediate.jpeg")} />
+            <Button
+              icon={<Icon name="code" color="#ffffff" />}
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0,
+              }}
+              title="Commencer le cours avancé"
+            />
+          </Card>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomePage;
+
+const styles = StyleSheet.create({
+  title: {
+    width: "100%",
+    padding: 10,
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "lightgray",
+  },
+  description: {
+    padding: 10,
+    textAlign: "justify",
+  },
+});
